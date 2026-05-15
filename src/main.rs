1cbf276c42944992400fad1c7504d6c65d84eefd
Ivan Sim <ihcsim@gmail.com>
2026-05-14 22:08:29 -0700

    add progress bar
    Signed-off-by: Ivan Sim <ihcsim@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,37 @@
 use anyhow::{Context, Result};
 use clap::Parser;
+use indicatif::ProgressBar;
 use std::io::{self, Write};
 use std::{fs, path::PathBuf};
 
 fn main() -> Result<()> {
     let args = Args::parse();
+    let pattern = args.pattern.clone();
     let path = &args.path;
     let content = fs::read_to_string(path)
         .with_context(|| format!("Failed to read file `{}`", path.display()))?;
 
+    let pb = ProgressBar::new(100);
     let mut buf = io::BufWriter::new(io::stdout());
+    let mut lines_total = 0;
+    let mut lines_matched = 0;
     for line in content.lines() {
+        lines_total += 1;
         if line.contains(&args.pattern) {
+            lines_matched += 1;
             writeln!(buf, "{}", line)?;
         }
+        pb.inc(1);
     }
+    pb.finish();
+
+    buf.flush()?;
+    println!(
+        "lines match: ({}/{}), time elapsed: {:?}",
+        lines_matched,
+        lines_total,
+        pb.elapsed()
+    );
     Ok(())
 }
 
