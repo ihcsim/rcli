c2bf3347916545b3cfa70b912870b61ee578c323
Ivan Sim <ihcsim@gmail.com>
2026-05-14 21:36:13 -0700

    add cli and args to parse string pattern in file
    Signed-off-by: Ivan Sim <ihcsim@gmail.com>

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use anyhow::{Context, Result};
+use clap::Parser;
+use std::{fs, path::PathBuf};
+
+fn main() -> Result<()> {
+    let args = Args::parse();
+    let path = &args.path;
+    let content = fs::read_to_string(path)
+        .with_context(|| format!("Failed to read file `{}`", path.display()))?;
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+    Ok(())
+}
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The pattern to look for
+    pattern: String,
+
+    /// The path to the file to read
+    path: PathBuf,
+}
+
+#[derive(Debug)]
+struct CustomError(String);
